575376b08323c35c4958c9fbb385e2f78ac9fb4c
John-Li-happy <63318597+John-Li-happy@users.noreply.github.com>
2020-10-27 13:23:12 -0400

    Initial Commit

---
+++ ForSudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForSudoku
+//
+//  Created by Zhaoyang Li on 10/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
